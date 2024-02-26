16d4ca1dd7bcab924486e667e642c34172cf603f
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-02-26 11:36:20 +0530

    Add reduced thickness debug image

--- process/edge.py
+++ process/edge.py
@@ -21,6 +21,7 @@ def detect(
     # Reduce the thickness of walls
     kernel = np.ones((3, 3), np.uint8)
     reduced_thickness = cv2.dilate(binary_image, kernel, iterations=thickness_reduction_iterations)
+    cv2.imshow(f"[DEBUG] Reduced Thickness | Iterations = {thickness_reduction_iterations}", reduced_thickness)
 
     # Single pixel morphological erosion (edge detection)
     kernel = np.ones((3, 3), np.uint8)
