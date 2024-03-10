bba6b114c8877694154af31a3950a990ca7765af
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-03-10 21:33:33 +0530

    Fix dunder name dunder main

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from process import merge
 from utility import path
 from utility import save
 
-if __name__ == "__main__":
+def main():
     filename = "fp1.png"
     io = path.io(filename)
     vertices = edge.detect(io.input, io.raw_vertices)
     merged_vertices = merge.close_vertices(io.input, io.merged_vertices, vertices, epsilon=12)
     save.vertices_as_txt(io.coordinates, merged_vertices)
+
+if __name__ == "__main__":
+    main()
