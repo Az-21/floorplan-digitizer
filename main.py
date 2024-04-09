30fbebe7db05a6c24b2e6e04c7075f1d07b0c3c8
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-04-09 12:37:37 +0530

    Fresh start, remove old processes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from process import edge, merge, svg
-from utility import save
+import clean.background
 import config.location as location
 
 
@@ -7,14 +6,7 @@ def main():
   filename = "fp1.png"
   io: location.IO = location.generate_io_paths(filename)
 
-  # Vertex detection
-  vertices = edge.detect(io.input, io.raw_vertices)
-  merged_vertices = merge.close_vertices(io.input, io.merged_vertices, vertices, epsilon=12)
-  save.vertices_as_txt(io.coordinates, merged_vertices)
-
-  # Wall erosion -> SVG
-  svg.detect_walls(io.input, io.walls)
-  svg.generate_wall_svg(io.walls, io.walls_svg)
+  clean.background.run(io)
 
 
 if __name__ == "__main__":
