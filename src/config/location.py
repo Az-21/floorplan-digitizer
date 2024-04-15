166abc54174ebfeb932df1dc6f031f0e293d2291
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-04-15 23:56:18 +0530

    Classify outputs into `image` and `data` subfolders
    This is in preparation of more components to be added. They will clutter the output. Final aim is to have `blender.py` and `insights.pdf` in root of output folder.

--- src/config/location.py
+++ src/config/location.py
@@ -13,6 +13,11 @@ class IO:
   coordinates: str
 
 
+# Categories of output
+IMAGE = "image"
+DATA = "data"
+
+
 def generate_io_paths(filename):
   # Generate input path
   input: str = f"input/{filename}"
@@ -20,12 +25,12 @@ def generate_io_paths(filename):
   # Generate outputs | Always save as .PNG
   base: str
   base, _ = os.path.splitext(filename)  # Discard extension
-  clean_background: str = f"output/{base}/clean-background.bmp"
-  clean_background_svg: str = f"output/{base}/clean-background.svg"
+  clean_background: str = f"output/{base}/{IMAGE}/clean-background.bmp"
+  clean_background_svg: str = f"output/{base}/{IMAGE}/clean-background.svg"
   blender_script: str = f"output/{base}/blender.py"
-  raw_vertices: str = f"output/{base}/raw-vertices.png"
-  merged_vertices: str = f"output/{base}/merged-vertices.png"
-  coordinates: str = f"output/{base}/vertex-coordinates.txt"
+  raw_vertices: str = f"output/{base}/{IMAGE}/raw-vertices.png"
+  merged_vertices: str = f"output/{base}/{IMAGE}/merged-vertices.png"
+  coordinates: str = f"output/{base}/{DATA}/vertex-coordinates.txt"
 
   # Return as object
   return IO(
@@ -43,5 +48,11 @@ def generate_output_folder(filename) -> None:
   base: str
   base, _ = os.path.splitext(filename)  # Discard extension
   path: str = f"output/{base}"
+  _generate_folder(path)
+  _generate_folder(os.path.join(path, IMAGE))
+  _generate_folder(os.path.join(path, DATA))
+
+
+def _generate_folder(path: str) -> None:
   if not os.path.exists(path):
     os.makedirs(path)
