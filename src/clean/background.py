4af6046a71b796f9fd19cd2656d1d840736e4190
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-06-01 18:59:20 +0530

    Add documentation for `background.py`

--- src/clean/background.py
+++ src/clean/background.py
@@ -1,3 +1,17 @@
+"""
+This module provides functionality to clean background elements from an image based on the intensity and thickness of pixels.
+It reads an input image, processes it to remove light strokes and redundant objects, and checks if the resulting image is blank.
+
+Dependencies:
+- `sys`: Standard library for system-specific parameters and functions.
+- `cv2`: OpenCV library for image processing.
+- `numpy`: NumPy library for numerical operations.
+- `loguru.logger`: For logging information.
+- `src.check.blank.is_image_blank`: Custom function to check if an image is blank.
+- `src.config.config.Config`: Custom class for configuration settings.
+- `src.config.location.IO`: Custom class for input/output paths.
+"""
+
 import sys
 import cv2
 import numpy as np
@@ -7,11 +21,23 @@ from src.config.config import Config
 from src.config.location import IO
 
 
-# Clean background elements based on the intensity and thickness of pixels
-def run(
-  io: IO,
-  config: Config,
-) -> None:
+def run(io: IO, config: Config) -> None:
+  """
+  Clean background elements based on the intensity and thickness of pixels.
+
+  Args:
+      io (IO): An instance of the IO class containing input/output paths.
+      config (Config): An instance of the Config class containing configuration settings.
+
+  Process:
+      1. Reads the input image.
+      2. Converts the image to grayscale.
+      3. Converts the grayscale image to a binary image using a threshold value to discard light strokes (e.g., doors, furniture).
+      4. Reduces the thickness of walls and then increases it to remove redundant objects.
+      5. Saves the processed image to the specified location.
+      6. Logs an info message indicating the location where the cleaned background has been saved.
+      7. Checks if the image is blank to prevent errors in the cropping process. If blank, logs an error message and exits the program.
+  """
   # Read image
   image = cv2.imread(io.input)
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
