fa910b283b4f3a0d73360dabdb351291eea503be
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-06-02 14:58:49 +0530

    Add documentation for `edge.py`

--- src/process/edge.py
+++ src/process/edge.py
@@ -1,3 +1,19 @@
+"""
+This module provides functionality for detecting vertices in an image and saving the results.
+It includes image processing steps such as converting to grayscale, thresholding, and morphological operations.
+
+Dependencies:
+- `cv2`: OpenCV library for image processing.
+- `numpy`: Library for numerical operations.
+- `loguru.logger`: For logging information.
+- `src.config.config.Config`: Custom class for configuration settings.
+- `src.config.location.IO`: Custom class for input/output paths.
+- `src.color`: Module for defining color constants.
+
+Functions:
+- `detect(io: IO, config: Config, debug=False, debug_vertex_position=False)`: Detects vertices in an image and saves the result.
+"""
+
 import cv2
 import numpy as np
 from loguru import logger
@@ -12,6 +28,30 @@ def detect(
   debug=False,
   debug_vertex_position=False,
 ):
+  """
+  Detects vertices in an image and saves the result.
+
+  Args:
+      io (IO): An instance of the IO class containing input/output paths.
+      config (Config): An instance of the Config class containing configuration settings.
+      debug (bool, optional): If True, enables debug mode to show intermediate steps. Defaults to False.
+      debug_vertex_position (bool, optional): If True, displays the coordinates of detected vertices. Defaults to False.
+
+  Process:
+      1. Reads the input image and converts it to grayscale.
+      2. Converts the grayscale image to a binary image using a threshold value.
+      3. Reduces the thickness of walls in the binary image using dilation.
+      4. Performs edge detection using morphological erosion.
+      5. Finds and draws contours in the edge-detected image.
+      6. Detects vertices from the contours and plots them on the original image.
+      7. Optionally shows intermediate steps and waits for user input if debug mode is enabled.
+      8. Saves the result image with detected vertices.
+      9. Logs the number of detected vertices and their overlay image path.
+      10. Returns a list containing the coordinates of the detected vertices.
+
+  Returns:
+      list: A list of coordinates of the detected vertices.
+  """
   # Read image
   image = cv2.imread(io.input)
   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
