bbc2faa4da546f6493b767c28195456b62687038
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-06-02 15:02:51 +0530

    Add documentation for `helper.py`

--- src/process/helper.py
+++ src/process/helper.py
@@ -1,6 +1,18 @@
 import os
 
 
-def remove_extension(filename):
-  base_name, extension = os.path.splitext(filename)
+def remove_extension(filename: str) -> str:
+  """
+  Removes the extension from a filename and returns the base name.
+
+  Args:
+      filename (str): The filename from which the extension will be removed.
+
+  Returns:
+      str: The base name of the file without the extension.
+
+  Example:
+      If the input filename is 'example.txt', the function will return 'example'.
+  """
+  base_name, _ = os.path.splitext(filename)
   return base_name
